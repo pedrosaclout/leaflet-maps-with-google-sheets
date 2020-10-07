73c7af5879a0907cbdccf8ee33ee1911c5794e66
Pedro Sá Couto <51131677+pedrosaclout@users.noreply.github.com>
2020-10-07 17:49:48 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dhK122E657jigZ6LCxmh73vPygRfE5v_p5knfWD5p-U/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBOrti2OWVRuZuUdbu_qVwVwKYYJU6hpk4';
