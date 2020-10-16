4907f217d06f26d209ce1c82b07929c0717ea617
Pedro Sá Couto <51131677+pedrosaclout@users.noreply.github.com>
2020-10-16 15:04:20 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -69,7 +69,7 @@ $(window).on('load', function() {
     var layerNamesFromSpreadsheet = [];
     var layers = {};
     for (var i in points) {
-      var pointLayerNameFromSpreadsheet = points[i].Group;
+      var pointLayerNameFromSpreadsheet = points[i].University;
       if (layerNamesFromSpreadsheet.indexOf(pointLayerNameFromSpreadsheet) === -1) {
         markerColors.push(
           points[i]['Marker Icon'].indexOf('.') > 0
