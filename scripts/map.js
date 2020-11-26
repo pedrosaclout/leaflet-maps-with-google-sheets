4842e948d47c604945448c6c3504eb9160082b87
rita <ritavsgraca@gmail.com>
2020-11-26 18:13:56 +0000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -102,7 +102,15 @@ $(window).on('load', function() {
     return layers;
   }
 
-
+  function setClipboard(value) {
+      var tempInput = document.createElement("input");
+      tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+      tempInput.value = value;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+  }
   /**
    * Assigns points to appropriate layers and clusters them if needed
    */
