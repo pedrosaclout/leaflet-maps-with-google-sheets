94a2b8d2ca38e93351e42826fb213d053d2a810c
rita <ritavsgraca@gmail.com>
2020-12-05 15:56:44 +0000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -133,7 +133,7 @@ var openallmarkers = L.layerGroup();
           '<h2>' + point['Name'] + '</h2>' +
           '<h3>' + point['Group'] + '</h3></div>' +
           '<p> ' + point['Description'] + ' </p>' +
-          (point['Website'] ? ('<h4 id="website"> <a class="desktopquery" href="' + point['Website'] + '" target="_blank">Website</a> </h4>') : '') +
+          (point['Website'] ? ('<h4 style="display: inline-block;"> <a class="desktopquery" href="' + point['Website'] + '" target="_blank">Website</a> </h4>') : '') +
           (point['Website'] ? ('<h4 id="website"> <a class="phonequery" href="' + point['Website'] + '" target="_blank"><span class="iconify" data-icon="mdi:web" data-inline="false"></span></a> </h4>') : '') +
           (point['LinkedIn'] ? ('<a class="linkedin desktopquery" href="' + point['LinkedIn'] + '" target="_blank"><h4>LinkedIn</h4></a>') : '') +
           (point['LinkedIn'] ? ('<a class="linkedin phonequery" href="' + point['LinkedIn'] + '" target="_blank"><span class="iconify" data-icon="mdi-linkedin" data-inline="false"></span></a>') : '') +
