b8b76f9c2066500c91198e0358a89d23b2a275a8
rita <ritavsgraca@gmail.com>
2021-03-14 21:19:23 +0000

    hash

--- scripts/map.js
+++ scripts/map.js
@@ -97,11 +97,8 @@ $(window).on('load', function() {
 
 var openallmarkers = L.layerGroup();
 
-// Assuming your map instance is in a variable called map
-  var allMapLayers = {
-                      'all': openallmarkers
-                      };
-  var hash = new L.Hash(map, allMapLayers);
+
+var hash = new L.Hash(map);
 
 
   /**
